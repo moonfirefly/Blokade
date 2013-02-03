cc48ed95ac9068201e6dc85671294d8c4dda2f1f
moonfirefly <dokoni@moonfirefly.com>
2013-02-03 02:35:58 -0500

    Xcode generated source and header files

---
+++ Blokade/Engine/GFXMenuInput.cpp
@@ -0,0 +1,9 @@
+//
+//  GFXMenuInput.cpp
+//  Blokade
+//
+//  Created by moonfirefly on 2013-02-02.
+//  Copyright (c) 2013 moonfirefly. All rights reserved.
+//
+
+#include "GFXMenuInput.h"
---
+++ Blokade/Engine/GFXMenuInput.hpp
@@ -0,0 +1,14 @@
+//
+//  GFXMenuInput.h
+//  Blokade
+//
+//  Created by moonfirefly on 2013-02-02.
+//  Copyright (c) 2013 moonfirefly. All rights reserved.
+//
+
+#ifndef __Blokade__GFXMenuInput__
+#define __Blokade__GFXMenuInput__
+
+#include <iostream>
+
+#endif /* defined(__Blokade__GFXMenuInput__) */
